dccb8fc9695ade10d75f613b8ea728f6763726ee
Melina <melveks@gmail.com>
2022-08-06 11:39:48 +0100

    Enviando projeto TodoList.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [itemsList, setItemsList] = useState([]);
+
+  function handleChangeInput(event) {
+    const inputTask = event.target.value;
+    setTask(inputTask);
+  }
+  function handleAddItemToList(event) {
+    event.preventDefault(); // desabilita o refresh da pagina ao enviar
+
+    if (!task) {
+      return;
+    }
+    setItemsList([...itemsList, task]); // <----- Copia todos os items ja existentes e entao adiociona o novo item
+    setTask("");
+  }
+
+  const deleteItem = (index) => () =>
+    setItemsList((itemsList) => itemsList.filter((_, i) => i !== index));
+
+  function handleToggleTaskCompletion(id: number) {
+    const taskIndex = task.findIndex((task) => {
+      return task.id === id;
+    });
+
+    const tempTasks = [...task];
+
+    tempTasks[taskIndex].isComplete = !tempTasks[taskIndex].isComplete;
+
+    setTask(tempTasks);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To Do List</h1>
+      <form className="form_task" onSubmit={handleAddItemToList}>
+        <input
+          className="text"
+          type="text"
+          placeholder="Adicione uma tarefa"
+          onChange={handleChangeInput}
+          value={task}
+        />
+        <button className="submit" type="submit">
+          {" "}
+          Adicionar
+        </button>
+        <ul className="todo_list">
+          {itemsList.map((item, index) => (
+            <li className="item_list" key={index}>
+              <div>
+                <input
+                  className="checkbox"
+                  type="checkbox"
+                  readOnly
+                  checked={task.isComplete}
+                  onClick={() => handleToggleTaskCompletion(task.id)}
+                />
+
+                <label
+                  className="list_task"
+                  type="text"
+                  placeholder="tarefa"
+                  onChange={handleChangeInput}
+                  value={item}
+                >
+                  {item}
+                </label>
+
+                <button className="button_delete" onClick={deleteItem(index)}>
+                  {" "}
+                  deletar
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </form>
     </div>
   );
 }
